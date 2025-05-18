bbad61aadd1967b62114ffc90c0d866337dc57cf
abhishek-01k <abhishek@push.org>
2025-05-18 15:59:18 +0530

    Added pwa functionalities

--- suimemeapp/next.config.js
+++ suimemeapp/next.config.js
@@ -1,10 +1,38 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+  runtimeCaching: [
+    {
+      urlPattern: /^https:\/\/.*\.(png|jpg|jpeg|svg|gif)$/,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'images',
+        expiration: {
+          maxEntries: 60,
+          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
+        },
+      },
+    },
+    {
+      urlPattern: /^https:\/\/.*\.(js|css)$/,
+      handler: 'StaleWhileRevalidate',
+      options: {
+        cacheName: 'static-resources',
+      },
+    },
+  ],
+});
+
+
 const nextConfig = {
   reactStrictMode: false,
   webpack: (config) => {
     // Add fallbacks for Node.js core modules
     config.resolve.fallback = { fs: false, net: false, tls: false };
-    
+
     return config;
   },
   typescript: {
@@ -23,4 +51,4 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+module.exports = withPWA(nextConfig);
